f7362b0a9a8067f0203f621e34af003f420bc097
Tessa Power <tessa@tessapower.co>
2022-05-06 18:32:51 +1200

    Add support for custom font and FontBook singleton

---
+++ space-resistance/src/main/java/space_resistance/assets/FontBook.java
@@ -0,0 +1,50 @@
+package space_resistance.assets;
+
+import java.awt.*;
+import java.io.IOException;
+
+public final class FontBook {
+    private static final String FONT_NAME = "Revamped.otf";
+
+    private static FontBook singleton = null;
+
+    private Font gameFont;
+
+    public static FontBook shared() {
+        if (singleton == null) {
+            singleton = new FontBook();
+        }
+
+        return singleton;
+    }
+
+    private FontBook() {
+        loadFonts();
+    }
+
+    private void loadFonts() {
+        try {
+            gameFont = Font.createFont(Font.TRUETYPE_FONT, AssetLoader.load(FONT_NAME));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(gameFont);
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public Font titleFont() {
+        return new Font(gameFont.getFontName(), Font.BOLD, 32);
+    }
+
+    public Font scoreBoardFont() {
+        return new Font(gameFont.getFontName(), Font.BOLD, 20);
+    }
+
+    public Font buttonFont() {
+        return new Font(gameFont.getFontName(), Font.PLAIN, 18);
+    }
+
+    public Font bodyFont() {
+        return new Font(gameFont.getFontName(), Font.PLAIN, 16);
+    }
+}
