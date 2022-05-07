edb87d5493aec1d74e3c6f2af079006be94adc96
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-08 00:49:31 +0300

    create background image class

---
+++ space-resistance/src/main/java/space_resistance/assets/sprites/Background.java
@@ -0,0 +1,12 @@
+package space_resistance.assets.sprites;
+
+import tengine.graphics.components.sprites.Sprite;
+
+import java.awt.*;
+import java.io.InputStream;
+
+public class Background extends Sprite {
+  public Background(InputStream is, Dimension dimension) {
+    super(is, dimension);
+  }
+}
