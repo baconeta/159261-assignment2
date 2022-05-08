f528d637cb112c0355ef92ddf34fe219fb2b9bab
Ali Soltanian Fard Jahromi <soltanianfardjahromi@gmail.com>
2022-05-08 23:02:27 +1200

    Player Shooting

--- space-resistance/src/main/java/space_resistance/player/PlayerControls.java
+++ space-resistance/src/main/java/space_resistance/player/PlayerControls.java
@@ -11,14 +11,16 @@ public record PlayerControls(Map<Integer, Action> controls) {
             Map.of(KeyEvent.VK_UP, Action.MOVE_UP,
                     KeyEvent.VK_DOWN, Action.MOVE_DOWN,
                     KeyEvent.VK_LEFT, Action.MOVE_LEFT,
-                    KeyEvent.VK_RIGHT, Action.MOVE_RIGHT
+                    KeyEvent.VK_RIGHT, Action.MOVE_RIGHT,
+                    KeyEvent.VK_ENTER, Action.SHOOT
             );
 
     private static final Map<Integer, Action> PLAYER_TWO_CONTROLS =
             Map.of(KeyEvent.VK_W, Action.MOVE_UP,
                     KeyEvent.VK_S, Action.MOVE_DOWN,
                     KeyEvent.VK_A, Action.MOVE_LEFT,
-                    KeyEvent.VK_D, Action.MOVE_RIGHT
+                    KeyEvent.VK_D, Action.MOVE_RIGHT,
+                    KeyEvent.VK_SHIFT, Action.SHOOT
             );
 
     public static PlayerControls controlsForPlayer(PlayerNumber playerNumber) {
