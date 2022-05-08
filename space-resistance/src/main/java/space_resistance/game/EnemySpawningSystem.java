94a3c23357fc79b6a463b22467f84cb4d4d529d0
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-08 22:53:26 +0300

    add in timing system and refactor wave data class

--- space-resistance/src/main/java/space_resistance/game/EnemySpawningSystem.java
+++ space-resistance/src/main/java/space_resistance/game/EnemySpawningSystem.java
@@ -3,27 +3,12 @@ package space_resistance.game;
 import space_resistance.actors.enemy.Enemy;
 import java.awt.*;
 
-// TODO The boss class must notify the spawning system when it dies so we can prepare and start the next wave
 public class EnemySpawningSystem {
     private int currentLevel;
     private EnemyWave currentWave;
-    private long timeBeforeWaveStart;
     private long timeLastWaveGenerated;
     private long timeLastEnemySpawned;
     private GameWorld gameWorld;
-
-    public SpawnState getCurrentState() {
-        return currentState;
-    }
-
-    private enum SpawnState {
-        PRE_WAVE,
-        SPAWNING,
-        BOSS,
-        POST_WAVE,
-        DEFAULT;
-    }
-
     private SpawnState currentState = SpawnState.DEFAULT;
 
     public EnemySpawningSystem(GameWorld gw) {
@@ -32,28 +17,64 @@ public class EnemySpawningSystem {
         generateEnemyWave();
     }
 
-    public void update() {}
+    public SpawnState getCurrentState() {
+        return currentState;
+    }
+
+    public void update() {
+        long currentTime = System.currentTimeMillis();
+        switch (currentState) {
+            case PRE_WAVE:
+                if ((currentTime - timeLastWaveGenerated) > currentWave.getMsBeforeWaveStarts()) {
+                    currentState = SpawnState.SPAWNING;
+                }
+                break;
+            case SPAWNING:
+                if ((currentTime - timeLastEnemySpawned) > currentWave.getMsBetweenEnemySpawn()) {
+                    SpawnEnemy();
+                }
+                break;
+            case BOSS:
+                // TODO Check if the boss exists in the world? or implement a notifier?
+                break;
+            case POST_WAVE:
+            case DEFAULT:
+                break;
+        }
+    }
 
     private void generateEnemyWave() {
-        // based on the currentLevel value, we need to create an Array of enemies that will spawn in the next wave
-        // once this function is called, we record the time so that we can check how much time has passed between
-        // the start of the wave and the first spawn of the wave. This enables there to be a tiny bit or breathing
-        // time between successive "levels".
-
-        // TODO add in creation of wave data
         currentWave = new EnemyWave(currentLevel);
         currentState = SpawnState.PRE_WAVE;
         timeLastWaveGenerated = System.currentTimeMillis();
     }
 
     public void bossDestroyed() {
+        currentState = SpawnState.POST_WAVE;
         currentLevel += 1;
         generateEnemyWave();
     }
 
     private void SpawnEnemy() {
-        Enemy enemy = currentWave.getRandomEnemyFromWave();
-        Point spawnLocation = new Point(0,0); // TODO generalise and randomise spawnLocation
-//        enemy.spawnAt(gameWorld, spawnLocation)
+        if (currentWave.getEnemiesRemaining() > 0) {
+            Enemy enemy = currentWave.getRandomEnemyFromWave();
+            Point spawnLocation = new Point(0, 0); // TODO generalise and randomise spawnLocation
+            //        enemy.spawnAt(gameWorld, spawnLocation)
+        } else {
+            currentState = SpawnState.BOSS;
+            //            Boss boss = currentWave.getBoss();
+            Point spawnLocation = new Point(0, 0); // TODO generalise and randomise spawnLocation?
+            //        boss.spawnAt(gameWorld, spawnLocation)
+
+        }
+        timeLastEnemySpawned = System.currentTimeMillis();
+    }
+
+    private enum SpawnState {
+        PRE_WAVE,
+        SPAWNING,
+        BOSS,
+        POST_WAVE,
+        DEFAULT;
     }
 }
