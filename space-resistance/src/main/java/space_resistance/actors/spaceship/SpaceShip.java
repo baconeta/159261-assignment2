db02f94c92cf3f350c11d60e8dd9424754773ad0
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-08 08:05:48 +0300

    remove redundant comments and refactor graphicEntity to graphic

--- space-resistance/src/main/java/space_resistance/actors/spaceship/SpaceShip.java
+++ space-resistance/src/main/java/space_resistance/actors/spaceship/SpaceShip.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 public class SpaceShip extends Actor {
     private static final String PLAYER_SHIP = "Player.png";
     // TODO: idk if this is correct? Gotta sort out sizing and scale
-    // I setup the player size to be smaller and the correct size
     private static final Dimension DIMENSION = new Dimension(64, 64);
 
     private final GameWorld world;
@@ -35,17 +34,14 @@ public class SpaceShip extends Actor {
     private SpaceShip(GameWorld world, Point origin, Player player) {
         this.world = world;
         this.player = player;
-        graphicEntity = initSprite();
-        setOrigin(origin); // Placed this line after graphicEntity = initSprite() to fix glitch of spaceship not spawning in specified area
+        graphic = initSprite();
+        setOrigin(origin);
     }
 
     private TGraphicCompound initSprite() {
         TGraphicCompound playerSprite = new TGraphicCompound(DIMENSION);
 
         Sprite spaceship = new PlayerShip(AssetLoader.load(PLAYER_SHIP), DIMENSION);
-
-        // Any other stuff you want here
-        setOrigin(origin);
 
         playerSprite.add(spaceship);
 
