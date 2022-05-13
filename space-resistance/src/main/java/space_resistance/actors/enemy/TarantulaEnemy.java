c6e9948eb38af9362aea5879225fab6e7c5d35cc
Ali Soltanian Fard Jahromi <soltanianfardjahromi@gmail.com>
2022-05-13 18:35:19 +1200

    Tarantula Enemy

--- space-resistance/src/main/java/space_resistance/actors/enemy/TarantulaEnemy.java
+++ space-resistance/src/main/java/space_resistance/actors/enemy/TarantulaEnemy.java
@@ -1,11 +1,64 @@
 package space_resistance.actors.enemy;
 
+import space_resistance.actors.bullet.TarantulaEnemyBullet;
+import space_resistance.assets.AssetLoader;
+import space_resistance.assets.sprites.TarantulaShip;
 import space_resistance.game.GameWorld;
+import tengine.graphics.components.TGraphicCompound;
+import tengine.graphics.components.sprites.Sprite;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TarantulaEnemy extends Enemy{
+
+    private final ArrayList<TarantulaEnemyBullet> bullets = new ArrayList<>();
+
+    public static TarantulaEnemy spawnAt(GameWorld world, Point origin) {
+        TarantulaEnemy enemy = new TarantulaEnemy(
+                world,
+                origin);
+
+        world.add(enemy);
+        return enemy;
+    }
+
     public TarantulaEnemy(GameWorld world, Point origin){
         super(world, origin);
+        graphic = initSprite();
+    }
+    public TGraphicCompound initSprite(){
+        SHIP_SPRITE = "TarantulaEnemy.png";
+        TGraphicCompound tarantulaEnemy = new TGraphicCompound(DIMENSION);
+        Sprite tarantulaShip = new TarantulaShip(AssetLoader.load(SHIP_SPRITE), DIMENSION);
+        tarantulaEnemy.add(tarantulaShip);
+
+        return tarantulaEnemy;
+    }
+    @Override
+    public void update() {
+        super.update();
+        for (TarantulaEnemyBullet bullet : bullets) {
+            bullet.update();
+        }
+        if (bullets.size() < (Math.random() * 40)) {
+            if (bullets.size() >= 1) {
+                // Delay shots
+                if (bullets.get(bullets.size() - 1).timeExisted() > 150) {
+                    bullets.add(TarantulaEnemyBullet.spawnAt(world, new Point(this.origin.x, this.origin.y + 20)));
+                }
+            } else {
+                bullets.add(TarantulaEnemyBullet.spawnAt(world, new Point(this.origin.x, this.origin.y + 20)));
+            }
+        } else {
+            final Random RANDOM = new Random();
+            if (bullets.get(bullets.size() - 1).timeExisted() > RANDOM.nextInt(2100 - 1300) + 1300) { // Shoot in bursts so that player isn't bombarded with constant shots from the enemy ship
+                for (TarantulaEnemyBullet bullet: bullets){
+                    bullet.destroy();
+                }
+                bullets.clear();
+            }
+        }
     }
 }
