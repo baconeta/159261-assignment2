03cb8890956869be4900b3c6895b76f2b95d5d7b
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-19 23:53:00 +0300

    add template boss enemy logic

--- space-resistance/src/main/java/space_resistance/actors/enemy/GoliathEnemy.java
+++ space-resistance/src/main/java/space_resistance/actors/enemy/GoliathEnemy.java
@@ -1,13 +1,27 @@
 package space_resistance.actors.enemy;
 
+import space_resistance.game.EnemySpawningSystem;
 import space_resistance.game.GameWorld;
 import tengine.geom.TPoint;
-import tengine.graphics.components.TGraphicCompound;
 
 import java.awt.*;
 
 public class GoliathEnemy extends Enemy {
-    public GoliathEnemy(EnemyType type, GameWorld world, TPoint origin, Dimension dimension, int scoreWorth) {
+    EnemySpawningSystem enemySpawningSystem;
+
+    public GoliathEnemy(
+            EnemyType type, GameWorld world, TPoint origin, Dimension dimension, int scoreWorth) {
         super(type, world, origin, dimension, scoreWorth);
+    }
+
+    public void spawnBoss(EnemySpawningSystem ess) {
+        enemySpawningSystem = ess;
+        super.spawnInWorld();
+    }
+
+    @Override
+    public void destroy() {
+        enemySpawningSystem.bossDestroyed();
+        super.destroy();
     }
 }
