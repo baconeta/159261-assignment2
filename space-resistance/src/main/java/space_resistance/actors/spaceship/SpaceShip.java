6017f7f32a75423a029b430486cd3874edece7f8
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-19 22:39:27 +0300

    undo change to player sprite bounds as it was correct

--- space-resistance/src/main/java/space_resistance/actors/spaceship/SpaceShip.java
+++ space-resistance/src/main/java/space_resistance/actors/spaceship/SpaceShip.java
@@ -74,7 +74,7 @@ public class SpaceShip extends Actor {
     protected TPhysicsComponent initPhysics() {
         boolean isStatic = false;
         boolean hasCollisions = true;
-        CollisionRect collisionRect = new CollisionRect(origin, new Dimension(graphic.dimension().width / 2, graphic.dimension().height));
+        CollisionRect collisionRect = new CollisionRect(origin, DIMENSION);
 
         // Feel free to change this if the speed isn't right
         velocity = new TVelocity(200, new TVector(0, 0));
