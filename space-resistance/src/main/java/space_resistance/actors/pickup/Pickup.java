dfb93ce09acd70bfab99fda69b28592e2fcfdd02
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-22 23:46:20 +0300

    add base files and images ready for Pickup spawning

--- space-resistance/src/main/java/space_resistance/actors/pickup/Pickup.java
+++ space-resistance/src/main/java/space_resistance/actors/pickup/Pickup.java
@@ -1,3 +1,38 @@
 package space_resistance.actors.pickup;
 
-public class Pickup {}
+import space_resistance.assets.sprites.PickupSprite;
+import space_resistance.game.Game;
+import space_resistance.game.GameWorld;
+import tengine.Actor;
+import tengine.geom.TPoint;
+import tengine.graphics.components.TGraphicCompound;
+import tengine.graphics.components.TGraphicObject;
+import tengine.graphics.components.shapes.TRect;
+
+import java.awt.*;
+
+public class Pickup extends Actor {
+    private final Dimension dimension;
+    private final PickupType type;
+
+    public Pickup(PickupType type, GameWorld world, TPoint origin, Dimension dimension) {
+        this.world = world;
+        this.type = type;
+        this.dimension = dimension;
+        destroyWhenOffScreen = true;
+        graphic = initSprite();
+        setOrigin(origin);
+    }
+
+    private TGraphicObject initSprite() {
+        // Pickup Sprite
+        TGraphicCompound pickup = new TGraphicCompound(dimension);
+        PickupSprite pickupSprite = new PickupSprite(type, this.dimension);
+        pickup.add(pickupSprite);
+        if (Game.DEBUG_MODE) {
+            pickup.add(
+                    new TRect(new Dimension((int) (dimension.width * 0.5), (int) (dimension.height * 0.5))));
+        }
+        return pickup;
+    }
+}
