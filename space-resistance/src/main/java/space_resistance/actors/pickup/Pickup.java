de0bbbb5f51e5fd1daedd7051e2d9e38c1ae3462
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-23 00:14:10 +0300

    add physics to pickups

--- space-resistance/src/main/java/space_resistance/actors/pickup/Pickup.java
+++ space-resistance/src/main/java/space_resistance/actors/pickup/Pickup.java
@@ -8,6 +8,10 @@ import tengine.geom.TPoint;
 import tengine.graphics.components.TGraphicCompound;
 import tengine.graphics.components.TGraphicObject;
 import tengine.graphics.components.shapes.TRect;
+import tengine.physics.TPhysicsComponent;
+import tengine.physics.collisions.shapes.CollisionRect;
+import tengine.physics.kinematics.TVector;
+import tengine.physics.kinematics.TVelocity;
 
 import java.awt.*;
 
@@ -21,7 +25,18 @@ public class Pickup extends Actor {
         this.dimension = dimension;
         destroyWhenOffScreen = true;
         graphic = initSprite();
+        physics = initPhysics();
+
         setOrigin(origin);
+    }
+
+    private TPhysicsComponent initPhysics() {
+        boolean isStatic = false;
+        boolean hasCollisions = true;
+        CollisionRect collisionRect = new CollisionRect(origin, graphic.dimension());
+        velocity = new TVelocity(0, new TVector(0, 0));
+
+        return new TPhysicsComponent(this, isStatic, collisionRect, hasCollisions);
     }
 
     private TGraphicObject initSprite() {
