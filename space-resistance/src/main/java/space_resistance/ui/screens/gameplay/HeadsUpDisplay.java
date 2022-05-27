b6f77a355eef07bbc6e9bb2d56f14724592a2a9e
Tessa Power <tessa@tessapower.co>
2022-05-27 18:14:21 +1200

    Remove score and health padding from HUD

--- space-resistance/src/main/java/space_resistance/ui/screens/gameplay/HeadsUpDisplay.java
+++ space-resistance/src/main/java/space_resistance/ui/screens/gameplay/HeadsUpDisplay.java
@@ -9,13 +9,6 @@ import tengine.graphics.components.text.TLabel;
 
 import java.awt.*;
 
-// TODO: Consider stacking the score and health above each other and placing them in the bottom left.
-//   This would allow placing a second player's score on the bottom right, and moving the pause instruction into the
-//   middle as well as making it smaller so it is subtler, e.g.
-//    |                                                        |
-//    | Health: 074                                Health: 085 |
-//    | Score: 00300           p: pause           Score: 01200 |
-//    +--------------------------------------------------------+
 public class HeadsUpDisplay extends TGraphicCompound {
     private static final int PAUSE_LABEL_OFFSET = 125;
     private static final int HEALTH_LABEL_OFFSET = 10;
@@ -32,13 +25,13 @@ public class HeadsUpDisplay extends TGraphicCompound {
         this.state = state;
 
         // Health Label
-        healthLabel.setText("Health: " + padHealthText(state.playerOne().healthRemaining()));
+        healthLabel.setText("Health: " + state.playerOne().healthRemaining());
         healthLabel.setFont(FontBook.shared().hudFont());
         healthLabel.setColor(Colors.Text.PRIMARY);
         healthLabel.setOrigin(new TPoint(HEALTH_LABEL_OFFSET, dimension.height - SCREEN_BOTTOM_OFFSET));
 
         // Score Label
-        scoreLabel.setText("Score: " + padScoreText(state.playerOne().score()));
+        scoreLabel.setText("Score: " + state.playerOne().score());
         scoreLabel.setFont(FontBook.shared().scoreFont());
         scoreLabel.setColor(Colors.Text.PRIMARY);
         scoreLabel.setOrigin(new TPoint(SCORE_LABEL_OFFSET, dimension.height - SCREEN_BOTTOM_OFFSET));
@@ -55,37 +48,15 @@ public class HeadsUpDisplay extends TGraphicCompound {
     @Override
     public void update(double dtMillis) {
         if (state.playerOne().shieldEnabled()) {
-            healthLabel.setText("Health: " + padHealthText(state.playerOne().healthRemaining() + state.playerOne().shieldHealth()));
+            healthLabel.setText("Health: " + state.playerOne().healthRemaining() + state.playerOne().shieldHealth());
             healthLabel.setColor(Colors.Text.SHIELD_ENABLED);
         } else {
-            healthLabel.setText("Health: " + padHealthText(state.playerOne().healthRemaining()));
+            healthLabel.setText("Health: " + state.playerOne().healthRemaining());
             healthLabel.setColor(Colors.Text.PRIMARY);
         }
 
-        scoreLabel.setText("Score: " + padScoreText(state.playerOne().score()));
+        scoreLabel.setText("Score: " + state.playerOne().score());
 
         super.update(dtMillis);
-    }
-
-    private String padScoreText(int score) {
-        if (score < 100) {
-            return "0000" + score;
-        } else if (score < 1000) {
-            return "00" + score;
-        } else if (score < 10000) {
-            return "0" + score;
-        }
-
-        return "" + score;
-    }
-
-    private String padHealthText(int health) {
-        if (health < 10) {
-            return "00" + health;
-        } else if (health < 100) {
-            return "0" + health;
-        }
-
-        return "" + health;
     }
 }
