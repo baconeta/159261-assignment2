7cddcdb1005725fe7d9fddc7474bf8bd63585efc
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-27 16:30:04 +0300

    implement second ship colour

--- space-resistance/src/main/java/space_resistance/assets/sprites/PlayerShip.java
+++ space-resistance/src/main/java/space_resistance/assets/sprites/PlayerShip.java
@@ -1,6 +1,7 @@
 package space_resistance.assets.sprites;
 
 import space_resistance.assets.AssetLoader;
+import space_resistance.player.PlayerNumber;
 import tengine.graphics.components.sprites.Sprite;
 
 import java.awt.*;
@@ -9,10 +10,12 @@ import java.io.InputStream;
 public class PlayerShip extends Sprite {
     private static final Dimension DIMENSION = new Dimension(64, 64);
     private static final InputStream SHIP_ASSET = AssetLoader.load("Player.png");
+    private static final InputStream PLAYER2_SHIP_ASSET = AssetLoader.load("Player2.png");
     private static final PlayerShip PLAYER_SHIP = new PlayerShip(SHIP_ASSET, DIMENSION);
+    private static final PlayerShip PLAYER_TWO_SHIP = new PlayerShip(PLAYER2_SHIP_ASSET, DIMENSION);
 
-    public static PlayerShip shipSprite() {
-        return PLAYER_SHIP;
+    public static PlayerShip shipSprite(PlayerNumber playerNumber) {
+        return playerNumber.equals(PlayerNumber.PLAYER_ONE) ? PLAYER_SHIP : PLAYER_TWO_SHIP;
     }
 
     private PlayerShip(InputStream is, Dimension dimension) {
