c5312f8d62269c07371902041191b58d5d93e142
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-29 01:00:28 +0300

    fix the odds for the pickups

--- space-resistance/src/main/java/space_resistance/game/GameWorld.java
+++ space-resistance/src/main/java/space_resistance/game/GameWorld.java
@@ -29,8 +29,8 @@ public class GameWorld extends World {
 
     // Pickups
     private static final double chanceHealth = 0.05;
-    private static final double chanceShield = 0.03;
-    private static final double chanceMissiles = 1;
+    private static final double chanceShield = 0.04;
+    private static final double chanceMissiles = 0.03;
 
     private final Notifier gameOverNotifier;
     private final GameState gameState;
