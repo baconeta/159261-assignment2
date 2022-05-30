bb6db835badd38350d88eeaa3be136ad8b5ef121
Tessa Power <tessa@tessapower.co>
2022-05-30 14:36:01 +1200

    Align switch statement cases

--- space-resistance/src/main/java/space_resistance/actors/enemy/EnemyConstants.java
+++ space-resistance/src/main/java/space_resistance/actors/enemy/EnemyConstants.java
@@ -24,46 +24,45 @@ public class EnemyConstants {
 
     public static int scoreValue(EnemyType type) {
         return switch (type) {
-            case MITE -> 100;
+            case MITE        -> 100;
             case GRASSHOPPER -> 300;
-            case TARANTULA -> 500;
-            case GOLIATH -> 1000;
+            case TARANTULA   -> 500;
+            case GOLIATH     -> 1000;
         };
     }
 
     public static int enemyHealth(EnemyType type, int level) {
         return switch (type) {
-            case MITE -> 50 + (level * 25);
+            case MITE        -> 50 + (level * 25);
             case GRASSHOPPER -> 100 + (level * 40);
-            case TARANTULA -> 200 + (level * 60);
-            case GOLIATH -> 1000 + (level * 500);
+            case TARANTULA   -> 200 + (level * 60);
+            case GOLIATH     -> 1000 + (level * 500);
         };
     }
 
     public static int enemySpeed(EnemyType type, int level) {
         return switch (type) {
-            case MITE -> 25 + (level * 5);
+            case MITE        -> 25 + (level * 5);
             case GRASSHOPPER -> 30 + (level * 10);
-            case TARANTULA -> 35 + (level * 10);
-            case GOLIATH -> 35 + (level * 5);
+            case TARANTULA   -> 35 + (level * 10);
+            case GOLIATH     -> 35 + (level * 5);
         };
     }
 
     public static int bulletSpeed(EnemyType type) {
         return switch (type) {
-            case MITE -> 400;
+            case MITE        -> 400;
             case GRASSHOPPER -> 450;
-            case TARANTULA -> 500;
-            case GOLIATH -> 600;
+            case TARANTULA   -> 500;
+            case GOLIATH     -> 600;
         };
     }
 
     public static int bulletDamage(EnemyType type) {
         return switch (type) {
-            case MITE -> 1;
-            case GRASSHOPPER -> 2;
-            case TARANTULA -> 2;
-            case GOLIATH -> 3;
+            case MITE                   -> 1;
+            case GRASSHOPPER, TARANTULA -> 2;
+            case GOLIATH                -> 3;
         };
     }
 }
